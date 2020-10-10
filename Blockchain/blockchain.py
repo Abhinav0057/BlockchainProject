35136474a60ca18409dce82c3d6927cba393d85f
Abhinav0057 <abhinav00aryl57@gmail.com>
2020-10-10 17:02:46 +0545

    proof of work problem

--- Blockchain/blockchain.py
+++ Blockchain/blockchain.py
@@ -2,6 +2,7 @@ import datetime  # for timestamp
 import hashlib  # for hasing the block
 import json  # for json files work
 from uuid import uuid4  # generate pseudo random numers..
+from flask import Flask 
 
 
 class Blockchain:  # defining our blockchain class
@@ -10,19 +11,16 @@ class Blockchain:  # defining our blockchain class
         self.trasactions = []
         # proof=1, for the genesis block.
         self.create_block(proof=1, previous_hash='0')
-        self.nodes = set()  # crates nodes unordederd set
+       # self.nodes = set()  # crates nodes unordederd set
 
     def create_block(self, proof, previous_hash):  # create a block
-        own_hash = hashlib.sha256(
-            str(proof**3 - proof**4).encode()).hexdigest()
-        block = {'index': len(self.chain)+1,
+                block = {'index': len(self.chain)+1,
                'timestamp': str(datetime.datetime.now()),
                'proof': proof,
                'previous_hash': previous_hash,
-               'own_hash': own_hash,
                'trasactions': self.trasactions}
-        self.chain.append(block)
-        return block
+                self.chain.append(block)
+                return block
 
     def get_previous_block(self):
         return self.chain[-1]
@@ -31,12 +29,14 @@ class Blockchain:  # defining our blockchain class
         new_proof = 1
         check_proof = False
         while check_proof is False:
-             hash_operation = hashlib.sha256(
-                 str(proof**3-previous_proof**3-proof+proof*3.14).encode()).hexdigest()
-            if hash_operation[:4] == '0000':
-                check_proof = True
+            hash_operation = hashlib.sha256(
+                 str(new_proof**3-previous_proof**3-new_proof+previous_proof*3.14).encode()).hexdigest()
+            if hash_operation[:4]!='0000':
+                check_proof = False
+                new_proof= new_proof+1
             else:
-                new_proof += 1
+                check_proof=True
+            
         return new_proof
 
     def hash(self,block):
@@ -51,38 +51,53 @@ class Blockchain:  # defining our blockchain class
                 return False
             previous_proof=previous_block['proof']
             proof=block['proof']
-            hash_operation=hashlib.sha256(str(proof**3-previous_proof**3-proof+proof*3.14).encode()).hexdigest()
+            hash_operation= hashlib.sha256(
+                 str(new_proof**3-previous_proof**3-new_proof+previous_proof*3.14).encode()).hexdigest()
             if hash_operation[:4]!='0000':
                 return False
             previous_block=block
             block_index+=1
         return True
-     def add_transactions(self,sender,reciver,amount):
-        self.trasactions.append({'sender':sender,
-                             'reciver':reciver,
-                             'amount':amount})
-        previous_block=self.get_previous_block()
-        return previous_block['index']+1
+     
     
-    def add_node(self,address):
-        parsed_url=urlparse(address)
-        self.nodes.add(parsed_url.netloc)
+   
         
-    def replace_chain(self):
-        network =self.nodes
-        longest_chain=None
-        max_length=len(self.chain)
-        for node in network:
-            response=request.get(f'http://{node}/get_chain')
-            length=response.json()['length']
-            chain=response.json()['chain']
-            if length>max_length and self.is_chain_valid(chain):
-                max_length=length
-                longest_chain=chain
-            if longest_chain:
-                self.chain=longest_chain
-                return True
-            return False
+    
+
+app=Flask(__name__)
+
+# creating node address on the port 5000
+
+
+
+
+blockchain=Blockchain()
+
+@app.route('/mine_block', methods=['GET'])
+def mine_block():
+    previous_block=blockchain.get_previous_block()
+    previous_proof=previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash=blockchain.hash(previous_block)
+ 
+    block = blockchain.create_block(proof, previous_hash)
+  
+    response={'message':"New block has been mined and added",
+               'index':block['index'],
+               'timestamp':block['timestamp'],
+               'proof': block['proof'],
+               'previous_hash': block['previous_hash'],
+               'trasactions':block['trasactions']}
+    return response, 200
+
+@app.route('/get_chain', methods=['GET'])
+def get_chain():
+    response={'chain':blockchain.chain,
+              'len':len(blockchain.chain)}
+    return response, 200
+    
+app.run(host='0.0.0.0', port=5000)
+
     
     
     
